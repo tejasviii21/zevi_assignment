c76554f72d31bcd3fea09e53d05d8b916d56e410
Tejasvi Jatic <121444242+tejasviii21@users.noreply.github.com>
2024-01-14 23:37:06 +0530

    filter

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,12 +1,9 @@
 import React, { useState, useEffect } from "react";
 import "./Products.scss";
 import ProductsNavBar from "../components/ProductsNavBar";
-
 import { BiChevronDown, BiChevronUp } from "react-icons/bi";
-
 import ProductsResults from "../components/ProductsResults";
 import { ratingBox } from "../Utilities/utilities";
-
 import { ProductType, fetchProducts } from "../FakerData/FakerData";
 
 interface MultipleFilterType {
@@ -38,285 +35,54 @@ const Products = () => {
   useEffect(() => {
     let filteredData: ProductType[] = [];
 
-    let filter1Applied = false;
-    let filter2Applied = false;
-    let filter3Applied = false;
+    const applyBrandFilter = multipleFilters.brand.includes(true);
+    const applyRatingFilter = multipleFilters.rating.includes(true);
+    const applyPriceFilter = multipleFilters.price.includes(true);
 
-    if (multipleFilters.brand.includes(true)) {
-      if (multipleFilters.brand[0]) {
-        tempProducts.forEach((product) => {
-          if (product.productName === "Incredible Frozen Table")
-            filteredData.push(product);
-        });
-      }
-      if (multipleFilters.brand[1]) {
-        tempProducts.forEach((product) => {
-          if (product.productName === "Tasty Wooden Car")
-            filteredData.push(product);
-        });
-      }
-      filter1Applied = true;
-    }
+    if (applyBrandFilter || applyRatingFilter || applyPriceFilter) {
+      filteredData = tempProducts.filter((product) => {
+        let matchesBrand = true;
+        let matchesRating = true;
+        let matchesPrice = true;
 
-     if (multipleFilters.rating.includes(true)) {
-      for (let i = 0; i < multipleFilters.rating.length; i++) {
-        if (multipleFilters.rating[i]) {
-          tempProducts.forEach((product) => {
-            if (product.productRating === i + 1) {
-              filteredData.push(product);
-            }
-          });
+        if (applyBrandFilter) {
+          matchesBrand = multipleFilters.brand.every(
+            (filter, index) => !filter || product.productName === ["Incredible Frozen Table", "Tasty Wooden Car"][index]
+          );
         }
-      }
-      filter2Applied = true;
-    }
 
-   if (multipleFilters.price[0]) {
-      tempProducts.forEach((product) => {
-        if (product.productDisPrice < 500) {
-          filteredData.push(product);
+        if (applyRatingFilter) {
+          matchesRating = multipleFilters.rating.some(
+            (filter, index) => filter && product.productRating === index + 1
+          );
         }
+
+        if (applyPriceFilter) {
+          const [under500, from1000to3000] = multipleFilters.price;
+          matchesPrice =
+            (under500 && product.productDisPrice < 500) ||
+            (from1000to3000 &&
+              product.productDisPrice >= 1000 &&
+              product.productDisPrice <= 3000);
+        }
+
+        return matchesBrand && matchesRating && matchesPrice;
       });
-
-      filter3Applied = true;
-    }
-
-    if (multipleFilters.price[1]) {
-      tempProducts.forEach((product) => {
-        if (
-          product.productDisPrice >= 1000 &&
-          product.productDisPrice <= 3000
-        ) {
-          filteredData.push(product);
-        }
-      });
-      filter3Applied = true;
-    }
-
-    if (filter1Applied || filter2Applied || filter3Applied) {
-      setProducts(filteredData);
     } else {
-      setProducts(tempProducts);
+      filteredData = tempProducts;
     }
-  }, [multipleFilters, tempProducts]);
 
-  console.log(multipleFilters);
+    setProducts(filteredData);
+  }, [multipleFilters, tempProducts]);
 
   return (
     <div className="products_page">
       <ProductsNavBar />
       <h2>Search Results</h2>
       <div className="filter_and_result_container">
-        <div className="filter_container">
-          <div className="">
-            <div
-              onClick={() => setShowBrandFilter((prev) => !prev)}
-              className="dropdown_container"
-            >
-              <div className="filter_title">BRAND</div>
-              {showBrandFilter ? (
-                <BiChevronUp size={24} />
-              ) : (
-                <BiChevronDown size={24} />
-              )}
-            </div>
-            {showBrandFilter && (
-              <div>
-                <div className="input_label_container">
-                  <input
-                    type="checkbox"
-                    name=""
-                    id=""
-                    checked={multipleFilters.brand[0]}
-                    onChange={() => {
-                      const updatedFilter = [...multipleFilters.brand];
-                      updatedFilter[0] = !updatedFilter[0];
-                      setMultipleFilters({
-                        ...multipleFilters,
-                        brand: updatedFilter,
-                      });
-                    }}
-                  />
-                  <label>Incredible Frozen Table</label>
-                </div>
-                <div className="input_label_container">
-                  <input
-                    type="checkbox"
-                    name=""
-                    id=""
-                    checked={multipleFilters.brand[1]}
-                    onChange={() => {
-                      const updatedFilter = [...multipleFilters.brand];
-                      updatedFilter[1] = !updatedFilter[1];
-                      setMultipleFilters({
-                        ...multipleFilters,
-                        brand: updatedFilter,
-                      });
-                    }}
-                  />
-                  <label>Tasty Wooden Car</label>
-                </div>
-              </div>
-            )}
-          </div>
-          <div className="filter_divider"></div>
-          <div className="">
-            <div
-              onClick={() => setShowPriceFilter((prev) => !prev)}
-              className="dropdown_container"
-            >
-              <div className="filter_title">PRICE RANGE</div>
-              {showPriceFilter ? (
-                <BiChevronUp size={24} />
-              ) : (
-                <BiChevronDown size={24} />
-              )}
-            </div>
-            {showPriceFilter && (
-              <div>
-                <div className="input_label_container">
-                  <input
-                    type="checkbox"
-                    name=""
-                    id=""
-                    checked={multipleFilters.price[0]}
-                    onChange={() => {
-                      const updatedFilter = [...multipleFilters.price];
-                      updatedFilter[0] = !updatedFilter[0];
-                      setMultipleFilters({
-                        ...multipleFilters,
-                        price: updatedFilter,
-                      });
-                    }}
-                  />
-                  <label>Under 500</label>
-                </div>
-                <div className="input_label_container">
-                  <input
-                    type="checkbox"
-                    name=""
-                    id=""
-                    checked={multipleFilters.price[1]}
-                    onChange={() => {
-                      const updatedFilter = [...multipleFilters.price];
-                      updatedFilter[1] = !updatedFilter[1];
-                      setMultipleFilters({
-                        ...multipleFilters,
-                        price: updatedFilter,
-                      });
-                    }}
-                  />
-                  <label>1000 to 3000</label>
-                </div>
-              </div>
-            )}
-          </div>
-          <div className="filter_divider"></div>
-          <div className="">
-            <div
-              onClick={() => setShowRatingFilter((prev) => !prev)}
-              className="dropdown_container"
-            >
-              <div className="filter_title">RATINGS</div>
-              {showRatingFilter ? (
-                <BiChevronUp size={24} />
-              ) : (
-                <BiChevronDown size={24} />
-              )}
-            </div>
-            {showRatingFilter && (
-              <div>
-                <div className="input_label_container">
-                  <input
-                    type="checkbox"
-                    name=""
-                    id=""
-                    checked={multipleFilters.rating[4]}
-                    onChange={() => {
-                      const updatedFilter = [...multipleFilters.rating];
-                      updatedFilter[4] = !updatedFilter[4];
-                      setMultipleFilters({
-                        ...multipleFilters,
-                        rating: updatedFilter,
-                      });
-                    }}
-                  />
-                  <label>{ratingBox(5)}</label>
-                </div>
-                <div className="input_label_container">
-                  <input
-                    type="checkbox"
-                    name=""
-                    id=""
-                    checked={multipleFilters.rating[3]}
-                    onChange={() => {
-                      const updatedFilter = [...multipleFilters.rating];
-                      updatedFilter[3] = !updatedFilter[3];
-                      setMultipleFilters({
-                        ...multipleFilters,
-                        rating: updatedFilter,
-                      });
-                    }}
-                  />
-                  <label>{ratingBox(4)}</label>
-                </div>
-                <div className="input_label_container">
-                  <input
-                    type="checkbox"
-                    name=""
-                    id=""
-                    checked={multipleFilters.rating[2]}
-                    onChange={() => {
-                      const updatedFilter = [...multipleFilters.rating];
-                      updatedFilter[2] = !updatedFilter[2];
-                      setMultipleFilters({
-                        ...multipleFilters,
-                        rating: updatedFilter,
-                      });
-                    }}
-                  />
-                  <label>{ratingBox(3)}</label>
-                </div>
-                <div className="input_label_container">
-                  <input
-                    type="checkbox"
-                    name=""
-                    id=""
-                    checked={multipleFilters.rating[1]}
-                    onChange={() => {
-                      const updatedFilter = [...multipleFilters.rating];
-                      updatedFilter[1] = !updatedFilter[1];
-                      setMultipleFilters({
-                        ...multipleFilters,
-                        rating: updatedFilter,
-                      });
-                    }}
-                  />
-                  <label>{ratingBox(2)}</label>
-                </div>
-                <div className="input_label_container">
-                  <input
-                    type="checkbox"
-                    name=""
-                    id=""
-                    checked={multipleFilters.rating[0]}
-                    onChange={() => {
-                      const updatedFilter = [...multipleFilters.rating];
-                      updatedFilter[0] = !updatedFilter[0];
-                      setMultipleFilters({
-                        ...multipleFilters,
-                        rating: updatedFilter,
-                      });
-                    }}
-                  />
-                  <label>{ratingBox(1)}</label>
-                </div>
-              </div>
-            )}
-          </div>
-        </div>
-        <ProductsResults products={products} />
+        {/* ... Your existing filter UI code ... */}
       </div>
+      <ProductsResults products={products} />
     </div>
   );
 };
